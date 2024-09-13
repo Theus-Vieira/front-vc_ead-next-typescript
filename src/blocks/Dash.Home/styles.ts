3ecee655fe20e86893f7a3727d835713e83194bb
Theus <matheusth.dev@gmail.com>
2024-09-13 14:27:48 -0300

    feat: home folder on home page mobile queries

--- src/blocks/Dash.Home/styles.ts
+++ src/blocks/Dash.Home/styles.ts
@@ -1,122 +1,102 @@
 import styled from "styled-components";
 
 export const Container = styled.section`
-  @media (min-width: 410px) {
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1rem;
+
+  & > h2 {
+    width: 100%;
+
+    font-size: 20pt;
+    text-align: left;
+  }
+
+  & > p {
+    width: 100%;
+
+    font-size: 9pt;
+    text-align: left;
   }
 
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 1024px) {
-  }
-
-  @media (min-width: 1200px) {
-    width: 100%;
-    height: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1rem;
-
     & > h2 {
-      width: 100%;
-
       padding: 0 4rem;
 
       font-size: 25pt;
-      text-align: left;
     }
 
     & > p {
-      width: 100%;
-
       padding: 0 4rem;
 
-      font-size: 12pt;
-      text-align: left;
+      font-size: 10pt;
     }
   }
 
-  @media (min-width: 1440px) {
+  @media (min-width: 1024px) {
+    & > p {
+      font-size: 12pt;
+    }
   }
 `;
 
 export const BoxCards = styled.section`
-  @media (min-width: 410px) {
-  }
+  width: 100%;
+
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  gap: 2rem;
+
+  margin-top: 3rem;
 
   @media (min-width: 768px) {
-  }
-
-  @media (min-width: 1024px) {
-  }
-
-  @media (min-width: 1200px) {
-    width: 100%;
-
-    display: flex;
-    justify-content: center;
     gap: 4rem;
-  }
-
-  @media (min-width: 1440px) {
   }
 `;
 
 export const Card = styled.section`
-  @media (min-width: 410px) {
+  width: 12rem;
+  height: 12rem;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  padding: 1rem;
+
+  border: 4px solid ${(props) => props.theme.txtPrimary};
+  border-radius: 2rem;
+
+  cursor: pointer;
+  transition: 1.2s;
+
+  & > svg {
+    font-size: 25pt;
+
+    margin-bottom: 1rem;
   }
 
-  @media (min-width: 768px) {
+  & > h3 {
+    font-size: 10pt;
   }
 
-  @media (min-width: 1024px) {
+  & > strong {
+    font-size: 10pt;
   }
 
-  @media (min-width: 1200px) {
-    width: 12rem;
-    height: 12rem;
+  &:hover {
+    transform: scale(1.1);
 
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
+    transition: 0.8s;
 
-    margin-top: 3rem;
-    padding: 1rem;
-
-    border: 4px solid ${(props) => props.theme.txtPrimary};
-    border-radius: 2rem;
-
-    cursor: pointer;
-    transition: 1.2s;
-
-    & > svg {
-      font-size: 25pt;
-
-      margin-bottom: 1rem;
-    }
-
-    & > h3 {
-      font-size: 10pt;
-    }
-
-    & > strong {
-      font-size: 10pt;
-    }
-
-    &:hover {
-      transform: scale(1.1);
-
-      transition: 0.8s;
-
-      border-color: ${(props) => props.theme.secondary};
-      color: ${(props) => props.theme.secondary};
-    }
-  }
-
-  @media (min-width: 1440px) {
+    border-color: ${(props) => props.theme.secondary};
+    color: ${(props) => props.theme.secondary};
   }
 `;
