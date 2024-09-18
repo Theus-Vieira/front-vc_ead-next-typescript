58e47bc13f30eea92df2715e41cd389ee484373d
Theus <matheusth.dev@gmail.com>
2024-09-18 09:32:25 -0300

    style: mobile queries for register form

--- src/blocks/RegisterForm/styles.ts
+++ src/blocks/RegisterForm/styles.ts
@@ -1,35 +1,17 @@
 import styled from "styled-components";
 
 export const Container = styled.form`
-  @media (min-width: 410px) {
-  }
+  width: 100%;
 
-  @media (min-width: 768px) {
-  }
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 1.5rem;
 
-  @media (min-width: 1024px) {
-  }
+  text-align: center;
 
-  @media (min-width: 1200px) {
-    width: 100%;
-
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: center;
-    gap: 1.5rem;
-
-    text-align: center;
-
-    & > button {
-      margin-top: 0.5rem;
-    }
-
-    & > span {
-      font-size: 9pt;
-    }
-  }
-
-  @media (min-width: 1440px) {
+  & > button {
+    margin-top: 0.5rem;
   }
 `;
