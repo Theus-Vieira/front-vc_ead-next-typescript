63b293a082e90e542f82e90a88b8d915ecfe710c
Theus <matheusth.dev@gmail.com>
2024-11-28 18:15:27 -0300

    refactor: simplify connection with socket

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -2,10 +2,10 @@ import { io } from "socket.io-client";
 import { IUser } from "@/types";
 import "dotenv/config";
 
-const ip = process.env.IP;
-const port = process.env.PORT;
-const isDev = process.env.IS_DEV === "true";
-const socketURL = process.env.SOCKET_UR;
+const ip = "192.168.1.14";
+const port = "3001";
+const isDev = false;
+const socketURL = "https://vceadchat.onrender.com";
 
 export const socket = (user: IUser) =>
   io(isDev ? `${ip}:${port}` : socketURL, {
